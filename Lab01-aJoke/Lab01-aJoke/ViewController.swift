30d6704ca14f2d27fb12b190fdad4e0245e381ac
Benjamin Dudra <144833560+MrHighball@users.noreply.github.com>
2024-09-09 14:45:38 -0400

    App made based on lab guide

--- Lab01-aJoke/Lab01-aJoke/ViewController.swift
+++ Lab01-aJoke/Lab01-aJoke/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var jokeLabel: UILabel!
+    @IBOutlet var punchlineLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showJoke(_ sender: UIButton){
+        print("Joke Button Pressed")
+        jokeLabel.text = "There are 3 types of people in the world"
+    }
+    @IBAction func showPunchLine(_ sender: UIButton){
+        print("Punchline buttone pressed")
+        punchlineLabel.text = "Those who can count, and those who can't"
+    }
 }
-
