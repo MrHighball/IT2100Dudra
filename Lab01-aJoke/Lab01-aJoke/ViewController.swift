e70830fc1cca7fe26c876c12f2adfd29dd001b8d
Benjamin Dudra <144833560+MrHighball@users.noreply.github.com>
2024-09-09 10:24:30 -0400

    LAB 1 (Made a multipurpose instead of a IOS App)

---
+++ Lab01-aJoke/Lab01-aJoke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab01-aJoke
+//
+//  Created by Benjamin Dudra on 9/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Lab01-aJoke/Lab01-aJokeTests/Lab01_aJokeTests.swift
+++ Lab01-aJoke/Lab01-aJokeTests/Lab01_aJokeTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Lab01_aJoke
 
 final class Lab01_aJokeTests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class Lab01_aJokeTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
