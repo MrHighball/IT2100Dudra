dbee7d450ad70ebb4175beb0d58cf82b7c32ae33
Benjamin Dudra <144833560+MrHighball@users.noreply.github.com>
2024-09-09 10:11:47 -0400

    Created Lab1

---
+++ Lab01-aJoke/Lab01-aJoke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab01-aJoke
+//
+//  Created by Benjamin Dudra on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab01-aJoke/Lab01-aJoke/Lab01_aJokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_aJokeApp.swift
+//  Lab01-aJoke
+//
+//  Created by Benjamin Dudra on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_aJokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
