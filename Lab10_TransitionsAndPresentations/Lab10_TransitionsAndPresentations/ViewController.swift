79d50c2f05c3ac4e0a4a561b02b0b8efa346c533
Benjamin Dudra <144833560+MrHighball@users.noreply.github.com>
2024-11-24 22:08:44 -0500

    lab10

--- Lab10_TransitionsAndPresentations/Lab10_TransitionsAndPresentations/ViewController.swift
+++ Lab10_TransitionsAndPresentations/Lab10_TransitionsAndPresentations/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemRed
+        button.setTitle("Tap me", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
 
 
+    @objc private func didTapButton() {
+        let vc = SecondViewController()
+        vc.modalTransitionStyle = .flipHorizontal
+        present(vc, animated: true)
+    }
 }
 
